8094249c4b99f44f6aeff53a56b98765a02e0ba0
Athul Krishna K <athulprgm.pycslearn@gmail.com>
2026-09-08 17:39:10 +0530

    fix

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,3 +1,6 @@
+/* eslint-env serviceworker */
+/* global importScripts, firebase */
+
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 
